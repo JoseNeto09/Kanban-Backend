ee44c13a4b96c34f26543724a0d3f628f3bef869
JoseNeto09 <joselopessobrinhoneto9@gmail.com>
2026-02-03 21:23:36 -0300

    vamos de deploy

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 
-import taskRoutes from "./routes/task.routes";
+import taskRoutes from "./routes/task.routes.js";
 
 dotenv.config();
 
